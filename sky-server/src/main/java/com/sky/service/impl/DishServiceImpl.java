35c39e2be076042b64ff5ec3c4532a312dd35556
yueyijessie <welhomejessie@gmail.com>
2023-12-14 15:34:55 +0800

    完成新增菜品，insert会影响dish和dish_flavor两张表

---
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -0,0 +1,51 @@
+package com.sky.service.impl;
+
+import com.sky.dto.DishDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.DishFlavor;
+import com.sky.mapper.DishFlavorMapper;
+import com.sky.mapper.DishMapper;
+import com.sky.service.DishService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class DishServiceImpl implements DishService {
+
+    @Autowired
+    private DishMapper dishMapper;
+
+    @Autowired
+    private DishFlavorMapper dishFlavorMapper;
+
+    /**
+     * 新增菜品和对应的口味
+     * @param dishDTO
+     */
+    @Transactional  // 需要操作两个表，需要保证事务一致性
+    public void addDish(DishDTO dishDTO){
+
+        Dish dish = new Dish();
+        BeanUtils.copyProperties(dishDTO,dish);
+        // 向菜品表插入1条数据
+        dishMapper.addDish(dish);
+
+        // 获取insert语句生成的主键值
+        Long dishId = dish.getId();
+
+        List<DishFlavor> flavors = dishDTO.getFlavors();
+        if (flavors != null && !flavors.isEmpty()){
+            // 遍历flavor插入dishId
+            flavors.forEach(dishFlavor -> {
+                dishFlavor.setDishId(dishId);
+            });
+            // 向口味表插入n条数据
+            dishFlavorMapper.insertBatch(flavors);
+        }
+
+    }
+}

cc94c66b2a8bdf921e30dd9f24e4daf6b24c0a11
yueyijessie <welhomejessie@gmail.com>
2023-12-27 15:52:42 +0800

    添加spring cache注解，用户端添加cacheable注解，管理端添加cacheEvict注解

--- sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
+++ sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -11,6 +11,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -32,6 +33,7 @@ public class SetmealController {
      */
     @PostMapping
     @ApiOperation("新增套餐")
+    @CacheEvict(cacheNames = "setmealCache", key = "#setmealDTO.categoryId") // 数据变更因此删除缓存，用户查询时没有再添加
     public Result createSetmeal(@RequestBody SetmealDTO setmealDTO){
         setmealService.create(setmealDTO);
         return Result.success();
@@ -46,6 +48,7 @@ public class SetmealController {
 
     @DeleteMapping
     @ApiOperation("批量删除套餐")
+    @CacheEvict(cacheNames = "setmealCache", allEntries = true) // 删除全部缓存数据
     public Result delete(@RequestParam List<Long> ids){
         setmealService.deleteByIds(ids);
         return Result.success();
@@ -61,6 +64,7 @@ public class SetmealController {
 
     @PutMapping
     @ApiOperation("修改套餐")
+    @CacheEvict(cacheNames = "setmealCache", allEntries = true)
     public Result update(@RequestBody SetmealDTO setmealDTO){
         setmealService.update(setmealDTO);
         return Result.success();
@@ -69,6 +73,7 @@ public class SetmealController {
 
     @PostMapping("/status/{status}")
     @ApiOperation("套餐启售停售")
+    @CacheEvict(cacheNames = "setmealCache", allEntries = true)
     public Result enableOrDisable(@PathVariable Integer status, Long id){
         setmealService.enableOrDisable(status, id);
         return Result.success();
